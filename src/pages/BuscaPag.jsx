185f708af7dba41ef8d3c2c572b18e6506cc8bf5
allanydias <allanydiasnave@gmail.com>
2026-05-18 22:03:57 -0300

    feat: adiciona página de busca

--- src/pages/BuscaPag.jsx
+++ src/pages/BuscaPag.jsx
@@ -5,7 +5,14 @@ function Busca() {
   const termo = searchParams.get('q');
 
   return (
-    <div style={{ padding: '2rem' }}>
+    <div style={{ 
+      padding: '2rem',
+      display: 'flex', 
+      flexDirection: 'column',
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      height: '100vh' 
+      }}>
       <h2>Nenhum resultado encontrado para: <strong>"{termo}"</strong></h2>
       <p>A busca ainda está em desenvolvimento.</p>
     </div>
